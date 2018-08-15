1b468c4759ac350ed9f447f4426fe0855744cb74
jneo8 <james851212@gmail.com>
2018-08-15 09:54:58 +0800

    refactoring func

--- 8_StringtoInteger_atoi/main.go
+++ 8_StringtoInteger_atoi/main.go
@@ -3,46 +3,39 @@ package string_to_integer_atoi
 import (
     "strconv"
     "math"
-    "fmt"
 )
 
 func myAtoi(str string) int {
-    negNum := 1
     resultStr := ""
-
-    for idx, s := range str {
-        s := string(s)
-        fmt.Printf("%s + ", resultStr)
-        fmt.Printf("%s = ", s)
-        if s == " " {
-            // pass
-        } else if s == "+" {
-            negNum *= 1
-        } else if s == "-" {
-            negNum *= -1
-        } else if s == "." {
-            resultStr += s
-        } else {
-            _, err := strconv.Atoi(s)
+    i := 0
+    stop := false
+    for i < len(str) && stop == false {
+        s := string(str[i])
+        if s == " " && len(resultStr) > 0 {
+            stop = true
+        } else if s != " " {
+            tmp_resultStr := resultStr + s
+            _, err := strconv.Atoi(tmp_resultStr)
             if err == nil {
-                resultStr += s
-            }
-            if err != nil &&  idx == 0 {
-                resultStr = "F"
+                resultStr = tmp_resultStr
+            } else {
+                if tmp_resultStr == "-" || tmp_resultStr == "+" {
+                    resultStr = tmp_resultStr
+                } else {
+
+                    stop = true
+                }
             }
         }
-        fmt.Printf("%s\n", resultStr)
-
+        i++
     }
-    result, _ := strconv.Atoi(resultStr)
-    result *= negNum
-    result_float64 := float64(result)
+    result, _ := strconv.ParseFloat(resultStr, 64)
 
-    if result_float64 > math.Pow(2, 31) - 1 {
-         result_float64 = math.Pow(2, 31) - 1
-    } else if result_float64 < - math.Pow(2, 31) {
-         result_float64 = - math.Pow(2, 31)
+    if result > math.Pow(2, 31) - 1 {
+        result = math.Pow(2, 31) - 1
+    } else if result < - math.Pow(2, 31) {
+        result = - math.Pow(2, 31)
     }
-    result_int := int(result_float64)
+    result_int := int(result)
     return result_int
 }
