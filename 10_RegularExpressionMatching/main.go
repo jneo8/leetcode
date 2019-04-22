3f24d9c98a98f93c0e86899ec9d150e049cb012e
jneo8 <jneo8lin@gmail.com>
2019-04-22 23:56:50 +0800

    solution

--- 10_RegularExpressionMatching/main.go
+++ 10_RegularExpressionMatching/main.go
@@ -1,88 +1,40 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func Split(r rune) bool {
-	return r == '*' || r == '.'
-}
-
-type Checker struct {
-	PList     []string
-	PIdx      int
-	Word      string
-	Character string
-	Count     int
-}
-
-func (c *Checker) Next() {
-	fmt.Printf("Next: %+v\n", c)
-
-	if c.PIdx == 0 && c.Word == "" {
-		c.Word = c.PList[0]
-	}
-
-	for {
-		c.PIdx += 1
-		switch p := c.PList[c.PIdx]; p {
-		case "*":
-			c.Character = p
-			break
-
-		case ".":
-			c.Character = p
-			break
-		default:
-			c.Word = p
-			c.Count = 0
-		}
-
-	}
-}
-
-func (c *Checker) Check(s string) bool {
-	fmt.Printf("Check: %v, %+v\n", s, c)
-
-	if c.Word == s {
-		if c.Count == 0 {
-			c.Count += 1
-			return true
-		}
-		if c.Count > 0 {
-			if c.Character == "*" {
-				c.Count += 1
-				return true
-			}
-			if c.Character == "." {
-				return false
-			}
-		}
-	} else {
-		c.Next()
-	}
-	return true
-}
-
 func isMatch(s string, p string) bool {
-	sList := strings.Fields(s)
-	fmt.Println(sList)
-
-	checker := Checker{
-		PList: strings.FieldsFunc(p, Split),
-		PIdx:  0,
-		Word:  "",
+	dp := make([][]bool, len(p)+1)
+	for i := 0; i < len(p)+1; i++ {
+		dp[i] = make([]bool, len(s)+1)
 	}
-
-	for idx, ss := range sList {
-		fmt.Printf("idx: %v, ss: %v\n", idx, ss)
-		b := checker.Check(ss)
-		if !b {
-			return false
+	dp[0][0] = true // dp[0][0..len(s)] = false, dp[1][0] = false, dp[0..len(p)][0] uncertain
+	for i := 2; i < len(p)+1; i++ {
+		// p[0] mustn't be *
+		if p[i-1] == '*' {
+			dp[i][0] = dp[i-2][0]
 		}
-
 	}
-
-	return true
+	for i := 1; i < len(p)+1; i++ {
+		for j := 1; j < len(s)+1; j++ {
+			if p[i-1] >= 'a' && p[i-1] <= 'z' {
+				dp[i][j] = p[i-1] == s[j-1] && dp[i-1][j-1]
+			} else if i >= 2 && p[i-1] == '*' && p[i-2] != '.' {
+				flag := dp[i-2][j]
+				for k := 1; k <= j; k++ {
+					if p[i-2] != s[j-k] {
+						break
+					}
+					flag = flag || dp[i-2][j-k]
+				}
+				dp[i][j] = flag
+			} else if i >= 2 && p[i-1] == '*' && p[i-2] == '.' {
+				flag := dp[i-2][j]
+				for k := 1; k <= j; k++ {
+					flag = flag || dp[i-2][j-k]
+				}
+				dp[i][j] = flag
+			} else {
+				dp[i][j] = dp[i-1][j-1]
+			}
+		}
+	}
+	return dp[len(p)][len(s)]
 }
--- 10_RegularExpressionMatching/main_test.go
+++ 10_RegularExpressionMatching/main_test.go
@@ -33,7 +33,7 @@ func Test_isMatch(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := isMatch(tt.args.s, tt.args.p); got != tt.want {
-				t.Errorf("isMatch() = %v, want %v", got, tt.want)
+				t.Errorf("%v isMatch() = %v, want %v", tt.args, got, tt.want)
 			}
 		})
 	}
